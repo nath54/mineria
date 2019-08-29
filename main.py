bcdcff82913782774c9ef2d25987c1622e51c7d7
nath54 <cerisara.nathan@gmail.com>
2019-08-29 08:56:12 +0100

    a

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ class Perso:
         self.acc=rxx(0.5)
         self.decc=rxx(0.1)
         self.grav=rxx(0.2)
-        self.accjump=rxx(6)
+        self.accjump=rxx(7)
         self.nbjump_tot=1
         self.nbjump=self.nbjump_tot
         #keys
@@ -77,19 +77,32 @@ class Perso:
         #inventaire
         self.inventaire=[] # pour chaque élément : 0=l'id de l'élément , 1=le nombre de bloc de cet élément possédé
         self.esb=0
+        #vie_endurance
+        self.vie_tot=500
+        self.vie=self.vie_tot
+        self.energie_tot=500
+        self.energie=self.energie_tot
+        self.issprint=False
+        self.dsprint=time.time()
+        self.tsprint=1.5
+        self.drempener=time.time()
+        self.trempener=0.03
+        self.dutilener=time.time()
+        self.tutilener=0.01
     def bouger(self,aa):
+        vitmax=self.vitmax
+        if self.issprint: vitmax=self.vitmax*2
         if aa=="Up" and time.time()-self.dkup>=self.t:
             self.dkup=time.time()
-            self.vity-=self.grav
         elif aa=="Down" and time.time()-self.dkdown>=self.t:
             self.dkdown=time.time()
             self.vity+=self.acc
         elif aa=="Left" and time.time()-self.dkleft>=self.t:
             self.dkleft=time.time()
-            self.vitx-=self.acc
+            if self.vitx>-vitmax: self.vitx-=self.acc
         elif aa=="Right" and time.time()-self.dkright>=self.t:
             self.dkright=time.time()
-            self.vitx+=self.acc
+            if self.vitx<vitmax: self.vitx+=self.acc
         elif aa=="Jump" and time.time()-self.dkjump>=self.tj and self.nbjump>0:
             self.dkjump=time.time()
             self.vity-=self.accjump
@@ -97,9 +110,27 @@ class Perso:
     def update(self,mape):
         if time.time()-self.dbg>=self.t:
             self.dbg=time.time()
+            #energie
+            if not self.issprint:
+                if time.time()-self.dsprint>=self.tsprint:
+                    if time.time()-self.drempener>=self.trempener:
+                        self.drempener=time.time()
+                        if self.energie<self.energie_tot: self.energie+=1
+            else:
+                if time.time()-self.dutilener>=self.tutilener:
+                    self.dutilener=time.time()
+                    self.energie-=1
+                    if self.energie<=0:
+                        self.energie=0
+                        self.issprint=False
             #physique
-            if self.vitx>self.vitmax: self.vitx=self.vitmax
-            elif self.vitx<-self.vitmax: self.vitx=-self.vitmax
+            vitmax=self.vitmax
+            if self.issprint: vitmax=self.vitmax*2
+            dd=rx(0.5)
+            if self.vitx>=vitmax+dd: self.vitx-=dd
+            elif self.vitx>vitmax: self.vitx=vitmax
+            elif self.vitx<-vitmax-dd:  self.vitx+=dd
+            elif self.vitx<-vitmax: self.vitx=-vitmax
             if self.vitx>0 and self.vitx>self.decc: self.vitx-=self.decc
             elif self.vitx>0: self.vitx=0.
             elif self.vitx<0 and self.vitx<-self.decc: self.vitx+=self.decc
@@ -119,18 +150,28 @@ class Perso:
                             my=y*mape.tc
                             mtx,mty=mape.tc,mape.tc
                             if self.rect.colliderect( (mx,my,mtx,mty) ):
-                                if self.py<my:
+                                if self.py+self.ty/1.5<my and self.py<my:
                                     self.py+=my-(self.py+self.ty)
                                     self.vity=0.
                                     self.nbjump=self.nbjump_tot
-                                else:
-                                    if self.px<=mx+mtx and self.px+self.tx>=mx+mtx:
-                                        self.px+=(mx+mtx)-self.px
-                                        self.vitx=0.
-                                    elif self.px+self.tx>=mx and self.px <=mx:
-                                        self.px-=(self.px+self.tx)-mx
-                                        self.vitx=0.
+                                    self.rect=pygame.Rect(self.px,self.py,self.tx,self.ty)
+                                elif self.py<my+mty and self.py+self.ty>my+mty:
+                                    self.py+=(my+mty)-self.py
+                                    self.vity=0
+                                    self.rect=pygame.Rect(self.px,self.py,self.tx,self.ty)
+                                if self.px<mx+mtx and self.px+self.tx>mx+mtx:
+                                    self.px+=(mx+mtx)-self.px
+                                    self.vitx=0.
+                                    self.rect=pygame.Rect(self.px,self.py,self.tx,self.ty)
+                                elif self.px+self.tx>mx and self.px <mx:
+                                    self.px-=(self.px+self.tx)-mx
+                                    self.vitx=0.
+                                    self.rect=pygame.Rect(self.px,self.py,self.tx,self.ty)
                                 self.rect=pygame.Rect(self.px,self.py,self.tx,self.ty)
+            if self.px<0: self.px=0
+            elif self.px>mape.mtx*mape.tc: self.px>mape.mtx*mape.tc
+            if self.py<0: self.py=0
+            elif self.py>mape.mty*mape.tc: self.py>mape.mty*mape.tc
             #camera
             self.cam=[-self.px+tex/2,-self.py+tey/2]
             #actions
@@ -177,12 +218,12 @@ class Mape:
         #de 101>101: herbe
         for y in range(1):
             for x in range(self.mtx): self.mape[x,y+100]=1
-        #de 102>130: herbe
-        for y in range(28):
+        #de 102>110: herbe
+        for y in range(8):
             for x in range(self.mtx): self.mape[x,y+101]=2
         #de 130>1000: pierre
-        for y in range(1000-130):
-            for x in range(self.mtx): self.mape[x,y+129]=3
+        for y in range(1000-110):
+            for x in range(self.mtx): self.mape[x,y+109]=3
         
         #ciel
         self.clciel=(0,100,150)
@@ -220,6 +261,11 @@ def aff(perso,mape,fps):
             fenetre.blit( pygame.transform.scale( emape[perso.inventaire[x][0]][2][0] , [tcx-t*2,tcy-t*2] ) , [xx+t,yy+t])
         pygame.draw.rect(fenetre,cl,(xx,yy,tcx,tcy),t)
         xx+=tcx+t
+    #barres  vie et énergie
+    pygame.draw.rect( fenetre , (255,0,0) , (rx(250),ry(870),int(perso.vie/perso.vie_tot*rx(600)),ry(15) ) , 0)
+    pygame.draw.rect( fenetre , (0,0,0) , (rx(250),ry(870),rx(600),ry(15) ) , rx(1))
+    pygame.draw.rect( fenetre , (255,200,0) , (rx(250),ry(853),int(perso.energie/perso.energie_tot*rx(600)),ry(15) ) , 0)
+    pygame.draw.rect( fenetre , (0,0,0) , (rx(250),ry(853),rx(600),ry(15) ) , rx(1))
     #textes affiché à l'écran ( sert surtout à débugger )
     fenetre.blit( font.render("fps="+str(fps),True,(255,255,255)), [rx(15),ry(15)])
     fenetre.blit( font.render("x="+str(int(perso.px)),True,(255,255,255)), [rx(15),ry(35)])
@@ -245,9 +291,22 @@ def main():
             if event.type==QUIT: exit()
             elif event.type==KEYDOWN:
                 if event.key==K_ESCAPE: encour=False
+                if event.key == K_RSHIFT:
+                    if perso.energie>0: perso.issprint=True
+                    perso.dsprint=time.time()
+            elif event.type==KEYUP:
+                if event.key == K_RSHIFT:
+                    perso.issprint=False
+                    perso.dsprint=time.time()
             elif event.type==MOUSEBUTTONDOWN:
                 if event.button==1: perso.isaction=True
                 if event.button==3: perso.isposer=True
+                if event.button==5:
+                    perso.esb-=1
+                    if perso.esb==-1: perso.esb=10
+                if event.button==4:
+                    perso.esb+=1
+                    if perso.esb==11: perso.esb=0
             elif event.type==MOUSEBUTTONUP:
                 if event.button==1:
                     perso.isaction=False
